7a5976850e34b0cb4f20da3996a7f35e32d6f2ba
BartekFranczak <91423411+BartekFranczak@users.noreply.github.com>
2022-12-13 16:02:05 +0100

    changed toreference

--- foo.hpp
+++ foo.hpp
@@ -9,9 +9,9 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > v;
 
-    for(Human h : people){
+    for(Human& h : people){
         h.birthday();
-        if(h.isMonster()){ v.emplace(v.begin(),'y');}
+        if(h.isMonster()){ v.emplace(v.begin(),'n');}
         else {v.emplace(v.begin(),'y');}
 
     }
